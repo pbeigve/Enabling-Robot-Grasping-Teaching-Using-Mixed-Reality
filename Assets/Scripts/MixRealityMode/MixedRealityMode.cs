6003c4beac038535cf6e9968439fe7581705efa0
marcosfdez99 <pbeigve@gmail.com>
2022-04-13 15:59:25 +0200

    prueba ordenador principal

--- Assets/Scripts/MixRealityMode/MixedRealityMode.cs
+++ Assets/Scripts/MixRealityMode/MixedRealityMode.cs
@@ -2,28 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Varjo.XR;
-
 public class MixedRealityMode : MonoBehaviour
 {
-    private int botton;
-    private void Start()
+   void Start()
     {
-        botton = 0;
+        VarjoMixedReality.StartRender();
     }
-    
+    private void Update()
+    {
+        
+    }
+    /* private int botton;
+     private void Start()
+     {
+         botton = 0;
+     }
 
-    
-    public void buttonpressed()
-        {
-if (botton == 0)
-{
-    VarjoMixedReality.StartRender();
-    botton = 1;
-}
-else
-{ 
-    VarjoMixedReality.StopRender();
-    botton = 0;
-}
-}
+
+
+     public void buttonpressed()
+         {
+ if (botton == 0)
+ {*/
+
+    /* botton = 1;
+ }
+ else
+ { 
+     VarjoMixedReality.StopRender();
+     botton = 0;
+ }
+ }
+ */
 }
