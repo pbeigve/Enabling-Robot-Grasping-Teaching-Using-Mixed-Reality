e0fbd075343bdc63abfed50a19d915dab4087d4f
marcosfdez99 <pbeigve@gmail.com>
2022-04-21 16:39:44 +0200

    revision

--- Assets/Scripts/ARmode.cs
+++ Assets/Scripts/ARmode.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class hello_world : MonoBehaviour
+using Varjo.XR;
+public class ARmode : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("perfeisto hermano\n");
+        VarjoMixedReality.StartRender();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("perfeisto hermano\n");
+        
     }
 }
--- Assets/Scripts/GraspBegin.cs
+++ Assets/Scripts/GraspBegin.cs
@@ -4,17 +4,62 @@ using UnityEngine;
 
 public class GraspBegin : MonoBehaviour
 {
-    public GameObject[] ObjectsGrasped;
-    private void Start()
+  
+       private Vector3 PosicionRelativa;
+    //struct de datos de colisiones
+    public struct ObjGR
     {
-       ObjectsGrasped = GameObject.FindGameObjectsWithTag("RHand");
-        foreach(GameObject ObjectGrasped in ObjectsGrasped)
+        public string Name;
+        public Vector3[] fingers;
+        public Vector3 RobotWrist;
+
+    }
+    public ObjGR[] dataGR;
+    public void Rellena_struct(string Name, GameObject[] fingers, Vector3 RobotWrist, ObjGR[] dataGR)
+    {
+        int i = 0;
+        int h = 0;
+        bool find = false;
+        while (i <= dataGR.Length & find!)
         {
-            Debug.Log("Posicion de " + gameObject.name + ", " + ObjectGrasped.transform.position);
-            Debug.DrawLine(ObjectGrasped.transform.position, ObjectGrasped.transform.position, Color.red);
+            if (dataGR[i].Name.Equals(Name))
+            {
+                foreach (GameObject finger in fingers)
+                {
+                    dataGR[i].fingers[h] = finger.transform.position;
+                }
+                dataGR[i].RobotWrist = RobotWrist;
+                find = true;
+            }
+            i++;
+        }
+        if (find!)
+        {
+            dataGR[i].Name = Name;
+            foreach (GameObject finger in fingers)
+            {
+                dataGR[i].fingers[h] = finger.transform.position;
+            }
+            dataGR[i].RobotWrist = RobotWrist;
+        }
+    }
+
+    
+     void Start()
+    {
+        GameObject[] fingerPos= GameObject.FindGameObjectsWithTag("RHand");
+        Vector3 WristPos = GameObject.Find("R_Palm").transform.position;
+        Rellena_struct(gameObject.name, fingerPos, WristPos, dataGR);
+
+        foreach (GameObject finger in fingerPos)
+        {
+            Debug.Log("Posicion de " + finger.name + ", " + finger.transform.position);
+            Debug.DrawLine(finger.transform.position, gameObject.transform.position, Color.red);
             //----------------------------------------------------PUNTOS RESPECTO AL SISTEMA----------------------
-            Vector3 Relativepos = ObjectGrasped.transform.position - gameObject.transform.position;
+            Vector3 Relativepos = finger.transform.position - gameObject.transform.position;
             Debug.Log("Posicion de " + gameObject.name + ", " + Relativepos);
-        }   
+            Debug.Log("posiion de la muneca en el struct "+ dataGR[0].RobotWrist);
+        }
+        
     }
 }
