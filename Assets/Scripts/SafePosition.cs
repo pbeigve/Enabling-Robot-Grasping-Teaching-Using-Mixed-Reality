3304b493a76903169d08554d060f7a929b773987
pbeigve <pbeigve@gmail.com>
2022-04-25 13:24:55 +0200

    .

--- Assets/Scripts/SafePosition.cs
+++ Assets/Scripts/SafePosition.cs
@@ -4,35 +4,17 @@ using UnityEngine;
 
 public class SafePosition : MonoBehaviour
 {
-    private Vector3 PosicionRelativa;
-    //struct de datos de colisiones
-    public struct ObjGR
+    private void Start()
     {
-        public string Name;
-        public List<Vector3> fingers;
-        public Vector3 RobotWrist;
-
+    
     }
-    public ObjGR[] dataGR;
-    public ObjGR[] Rellena_struct(string Name, List<Vector3> fingers, Vector3 RobotWrist, List<ObjGR> dataGR)
-    {
-        bool find = false;
-        int i = 0;
+    
 
-        while(find! or i<=dataGR.LastIndexOf)
-            {
-            if()
-        }
-        if (dataGR.Name.Contains(Name))
-            a1.Name.Add(Name);
-        foreach (Vector3 finger in fingers)
-        {
-            _
-            }
-        this.RobotWrist = RobotWrist;
-        return dataGR;
-    }
-    void OnCollisionStay(Collision collision)
+}
+   
+
+
+    /*void OnCollisionStay(Collision collision)
     {
         GameObject.Find("R_Palm");
         new a1(ObjGR(collision.collider, collision.contacts,   ))
@@ -44,5 +26,5 @@ public class SafePosition : MonoBehaviour
             // Visualize the contact point
             Debug.DrawRay(contact.point, contact.normal, Color.white);
         }
-    }
-}
+    }*/
+
